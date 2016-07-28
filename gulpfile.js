450bfb6e76850a581c58e0bd0f56eef1557ea1a2
Hashrocket Workstation <dev@hashrocket.com>
2016-07-28 16:43:03 -0400

    Creates dist directory and adds handlebars to the process of template creation

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,46 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var handlebars = require("gulp-compile-handlebars");
 var webserver = require("gulp-webserver");
 
-gulp.task("sass", function () {
+gulp.task("sass", function() {
     return gulp.src("./sass/**/*.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./css"));
+        .pipe(concat("./dist/css"));
+});
+
+gulp.task("hbs", function() {
+    var templateData = {};
+    var options = {
+        partials: {
+            "Standard Signature Block": ""
+        },
+        helpers: {
+            dateHelperFormat: function () {},
+            eq: function () {},
+            prompt: function () {},
+            dateHelperAddDaysToCurrentDate: function () {},
+            cannedText: function () {}
+        }
+    };
+
+    return gulp.src("templates/**/*.hbs")
+        .pipe(handlebars(templateData, options))
+        .pipe(rename(function(path) {
+            path.extname = ".html";
+        }))
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task("webserver", function() {
-    gulp.src(".")
+    gulp.src("dist")
         .pipe(webserver({
             livereload: true,
-            directoryListing: true,
             open: true
         }));
 });
 
-gulp.task("build", ["sass"]);
+gulp.task("build", ["sass", "hbs"]);
